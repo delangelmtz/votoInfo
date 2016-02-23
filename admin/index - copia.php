11952d34a32dca5a38f1d250da02d2820b796057
delangelmtz <delangelmtz@gmail.com>
2016-02-23 06:34:28 -0600

    asdasasdasas

---
+++ admin/index - copia.php
@@ -0,0 +1,54 @@
+<?php
+
+
+echo "hola";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+	<title>Administración Voto Informado</title>
+	<!-- LIBRERIAS css -->
+	<link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<style type="text/css">
+		.alignRgiht{
+			text-align: right;
+		}
+
+	</style>
+
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+    		<h1>My First Bootstrap Page</h1>
+    		<p>Resize this responsive page to see the effect!</p> 
+  		</div>
+		<div class="row">
+			<div class="col-xs-12">12
+			</div>
+			<div class="col-xs-6">
+				<h4>Usuarios</h4>
+			</div>
+			<div class="col-xs-6 alignRgiht">
+				<div class="btn btn-primary"><span class="glyphicon glyphicon-plus"></span> Nuevo</div>
+				<div class="btn btn-success"><span class="glyphicon glyphicon-pencil"></span> Editar</div>
+				<div class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span> Eliminar</div>
+			</div>
+			<div class="col-xs-4">4
+			</div>
+			<div class="col-xs-8">8
+			</div>
+			<div class="col-xs-4">4
+			</div>
+			<div class="col-xs-4">4
+			</div>
+			<div class="col-xs-4">4
+			</div>
+		</div>
+	</div>
+	<!-- LIBRERIAS javascript -->
+	<link href="../lib/bootstrap/js/bootstrap.min.js" rel="stylesheet" type="text/javascript" />
+</body>
+</html>
